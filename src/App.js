499513a902ad7615d1705aad5459bae084b697da
raysunwar <ray@whitehatagency.com.au>
2020-04-10 07:54:41 +0545

    useCallback Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import ParentComponent from './components/ParentComponent';
 function App() {
   return (
     <div className="App">
+      <ParentComponent />
     </div>
   );
 }
