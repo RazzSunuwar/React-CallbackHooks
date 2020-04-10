c20f1de4836e8f86d98a6ddbc083e05996fde4b4
raysunwar <ray@whitehatagency.com.au>
2020-04-10 15:55:13 +0545

    useInput Custom Hook

--- custom-hook/src/App.js
+++ custom-hook/src/App.js
@@ -4,14 +4,17 @@ import DocTitleOne from './components/DocTitleOne';
 import DocTitleTwo from './components/DocTitleTwo';
 import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
+import UserForm from './components/UseForm';
 
 function App() {
   return (
     <div className="App">
       {/* <DocTitleOne />
       <DocTitleTwo /> */}
-      <CounterOne />
-      <CounterTwo />
+      {/* <CounterOne />
+      <CounterTwo /> */}
+
+      <UserForm />
     </div>
   );
 }
